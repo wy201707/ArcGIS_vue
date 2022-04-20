37b5133e0fdc0041ac7d88f47cae2da31412cad4
wy <2639571439@qq.com>
2022-04-20 20:55:28 +0800

    -A

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="sceneview">
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {loadModules} from 'esri-loader'
+// import {loadScript, loadCss, loadModules} from 'esri-loader'
+// import * as esriLoader from "esri-loader";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  methods: {
+    //创建三维场景
+      __createSceneView:function(){
+          let options={
+            url:'https://js.arcgis.com/4.14/',
+            css:'https://js.arcgis.com/4.14/esri/themes/light/main.css'
+          }
+          loadModules(["esri/Map", "esri/views/SceneView"],options).then(([Map,SceneView])=>{
+            const map = new Map({
+              basemap: "streets",
+              ground: "world-elevation"
+            });
+            const view = new SceneView({
+              container: "sceneview", // Reference to the DOM node that will contain the view
+              map: map // References the map object created in step 3
+            });
+
+            console.log(view)
+          })
+
+      }
+  },
+  mounted:function(){
+   /*  if(!esriLoader.isLoaded()){
+      
+      esriLoader.bootstrap(
+          err => {
+            if (err) {
+              console.error(err);
+            } else {
+              this.importEsri();
+            }
+          },
+          {
+            url: "/static/arcgis_js_v49_api/arcgis_js_api/library/4.9/init.js"
+          }
+        );
+      } else {
+        this.importEsri();
+      }
+
+     */
+    // loadScript({ url: 'https://js.arcgis.com/4.23/' })
+    // loadCss('https://js.arcgis.com/4.23/esri/themes/light/main.css')
+
+     this.__createSceneView();
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  body{
+    margin: 0;
+  }
+#sceneview{
+  position: absolute;
+  width:100%;
+  height: 100%;
 }
 </style>
