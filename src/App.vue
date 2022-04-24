57719512b425cd0c132297abc213fa52ccc499b8
wy <2639571439@qq.com>
2022-04-24 09:26:29 +0800

    增加vscode配置（规范化代码格式）

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,38 @@
 <template>
-  <div id="sceneview">
-
-  </div>
+    <div id="sceneview"></div>
 </template>
 
 <script>
-import {loadModules} from 'esri-loader'
+import { loadModules } from 'esri-loader';
 // import {loadScript, loadCss, loadModules} from 'esri-loader'
 // import * as esriLoader from "esri-loader";
 
 export default {
-  name: 'App',
-  components: {
-    
-  },
-  methods: {
-    //创建三维场景
-      __createSceneView:function(){
-          let options={
-            url:'https://js.arcgis.com/4.14/',
-            css:'https://js.arcgis.com/4.14/esri/themes/light/main.css'
-          }
-          loadModules(["esri/Map", "esri/views/SceneView"],options).then(([Map,SceneView])=>{
-            const map = new Map({
-              basemap: "streets",
-              ground: "world-elevation"
-            });
-            const view = new SceneView({
-              container: "sceneview", // Reference to the DOM node that will contain the view
-              map: map // References the map object created in step 3
-            });
+    name: 'App',
+    components: {},
+    methods: {
+        //创建三维场景
+        __createSceneView: function () {
+            let options = {
+                url: 'https://js.arcgis.com/4.14/',
+                css: 'https://js.arcgis.com/4.14/esri/themes/light/main.css',
+            };
+            loadModules(['esri/Map', 'esri/views/SceneView'], options).then(([Map, SceneView]) => {
+                const map = new Map({
+                    basemap: 'streets',
+                    ground: 'world-elevation',
+                });
+                const view = new SceneView({
+                    container: 'sceneview', // Reference to the DOM node that will contain the view
+                    map: map, // References the map object created in step 3
+                });
 
-            console.log(view)
-          })
-
-      }
-  },
-  mounted:function(){
-   /*  if(!esriLoader.isLoaded()){
+                console.log(view);
+            });
+        },
+    },
+    mounted: function () {
+        /*  if(!esriLoader.isLoaded()){
       
       esriLoader.bootstrap(
           err => {
@@ -56,19 +51,21 @@ export default {
       }
 
      */
-    // loadScript({ url: 'https://js.arcgis.com/4.23/' })
-    // loadCss('https://js.arcgis.com/4.23/esri/themes/light/main.css')
+        // loadScript({ url: 'https://js.arcgis.com/4.23/' })
+        // loadCss('https://js.arcgis.com/4.23/esri/themes/light/main.css')
 
-     this.__createSceneView();
-  }
-}
+        this.__createSceneView();
+    },
+};
 </script>
 
 <style>
-  html,body,#sceneview{
-  position: relative;
-  width:100%;
-  height: 100%;
-  margin: 0;
+html,
+body,
+#sceneview {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    margin: 0;
 }
 </style>
